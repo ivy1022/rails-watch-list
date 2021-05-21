236a9b60717c7651087926e804ee710c6998246a
Ayaka Asano <yoonhak.x.aa@gmail.com>
2021-05-21 11:22:05 +0900

    saved

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find_by(id: params[:list_id])
+    # @bookmark = Bookmark.new(list_id: @list.id, movie_id: @movie.id)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.new(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+end
