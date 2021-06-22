dc2b01fc724db09bb34f398ba42770098242ce1e
Ayaka Asano <yoonhak.x.aa@gmail.com>
2021-06-22 21:07:36 +0900

    changed index

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'simple_form'
 gem 'faker'
 gem 'cloudinary', '~> 1.16.0'
 gem 'jquery-rails'
+gem 'bootstrap'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
+require('jquery')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -23,36 +24,3 @@ import 'bootstrap';
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 // console.log("Hello from app/javascript/packs/application.js!");
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
-
-
-$(document).ready(function () {
-  $("#images").skippr({
-    // スライドショーの変化 ("fade" or "slide")
-    transition : 'slide',
-    // 変化に係る時間(ミリ秒)
-    speed : 1000,
-    // easingの種類
-    easing : 'easeOutQuart',
-    // ナビゲーションの形("block" or "bubble")
-    navType : 'block',
-    // 子要素の種類("div" or "img")
-    childrenElementType : 'div',
-    // ナビゲーション矢印の表示(trueで表示)
-    arrows : true,
-    // スライドショーの自動再生(falseで自動再生なし)
-    autoPlay : true,
-    // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 3000,
-    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
-    keyboardOnAlways : true,
-    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
-    hidePrevious : false
-  });
-});
-
-import { Swiper } from '../components/swiper';
-document.addEventListener('turbolinks:load',() => {
-  swiper();
-});
