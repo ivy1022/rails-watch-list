9ebf5cdccbab34734d822d55f960365d0a54b813
Ayaka Asano <yoonhak.x.aa@gmail.com>
2021-06-23 21:52:39 +0900

    Fixed bookmark page

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,3 +4,42 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
+const results = document.querySelector("#results");
+
+const searchMovies = (query) => {
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      data.Search.forEach((result) => {
+        const movie = `<li class="list-inline-item">
+          <img src="${result.Poster}" alt="">
+          <p>${result.Title}</p>
+        </li>`;
+        results.insertAdjacentHTML("beforeend", movie);
+      });
+    });
+};
+
+const form = document.querySelector('#search-movies');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // <-- to prevent <form>'s native behaviour
+  const input = event.currentTarget.querySelector('.form-control');
+  results.innerHTML = '';
+  searchMovies(input.value);
+});
+
+
+const searchAlgoliaPlaces = (event) => {
+  fetch("https://places-dsn.algolia.net/1/places/query", {
+    method: "POST",
+    body: JSON.stringify({ query: event.currentTarget.value })
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.hits); // Look at local_names.default
+    });
+};
+
+const input = document.querySelector("#search");
+input.addEventListener("keyup", searchAlgoliaPlaces);
